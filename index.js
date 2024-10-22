e39af68df0460623199f20af8087a5acb7100612
JescaNgomane <ntavasingomane72@gmail.com>
2024-10-22 19:57:04 +0200

    Basic form handling and typewriter plugin

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function generator(event) {
+  event.preventDefault();
 
+  let resultElement = document.querySelector("#results");
+  new Typewriter("#results", {
+    strings: "Wearable Medical Devices and Remote Patient Monitoring",
+    autoStart: true,
+    delay: 10,
+    cursor: "",
+  });
+}
+
+let formElement = document.querySelector("#generate-result");
+formElement.addEventListener("submit", generator);
