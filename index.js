23a1123e40773f6e7f8aa3ba4c1ba8ebb4c3f2ad
JescaNgomane <ntavasingomane72@gmail.com>
2024-10-23 20:43:17 +0200

    AI API integration

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-function generator(event) {
-  event.preventDefault();
-
-  let resultElement = document.querySelector("#results");
+function displayResults(response) {
+  console.log("loaded");
   new Typewriter("#results", {
-    strings: "Wearable Medical Devices and Remote Patient Monitoring",
+    strings: response.data.answer,
     autoStart: true,
     delay: 10,
     cursor: "",
   });
+}
+
+function generator(event) {
+  event.preventDefault();
+
+  let inputElement = document.querySelector("#search-bar");
+  let apiKey = "8b28ob4022ta8eb8ef00a2f473170403";
+  let prompt = `user instructions: Generate the information about ${inputElement.value}`;
+  let context =
+    "You are an AI full of technology knowldge and biomedical engineering , please Make sure to provide a clear and precise answer about information on the medical device separate each fact in it's line using HTML </br> with a heading in paragraphs. Make sure to use the user instructions";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("loading.....");
+
+  axios.get(apiUrl).then(displayResults);
+
+  let resultElement = document.querySelector("#results");
 }
 
 let formElement = document.querySelector("#generate-result");
